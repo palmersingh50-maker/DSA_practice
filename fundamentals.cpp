6f4aa499293502696ce62587f2d74600fb1218c5
Palmer singh <palmersingh@Palmers-MacBook-Air.local>
2026-09-04 22:54:52 +0530

    Added first DSA practice

---
+++ fundamentals.cpp
@@ -0,0 +1,76 @@
+// #include<iostream>
+// using namespace std;
+// int main (){
+//     int a;
+//     cin>>a;
+//     if (a%2==0){
+//         cout<<"the number is even";
+
+//     }
+//     else{
+//         cout<<"the number is odd";
+
+//     }
+//     return 0;
+
+
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     // int a;
+//     // cin>>a;
+//     for (int i=1;i<19;i++){
+//         cout<<"hello"<<endl;
+
+//     }
+//     return 0;
+
+// }
+// #include <iostream>
+// using namespace std;
+// int main(){
+    
+//     int sum=0;
+// int i;
+//     for (i=0;i<=10;i++){
+//         sum+=i;
+       
+
+//     } cout<<sum<<endl;
+//     return 0;
+// }
+// sum of first 10 even numbers;
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n=0;
+//     int i;
+//     for (int i=0;i<=10;i++){
+//         n+=1;
+//      cout<<n<<endl;
+// }
+//     return 0;
+
+// }
+#include <iostream>
+using namespace std;
+int main (){
+    int n=0;
+
+    for (int i =0;i<=20;i++){
+    
+
+    
+    if (i%2==0){
+        cout<<i<<endl;
+        n+=i;
+    }
+
+    
+    
+}
+cout<<n;
+    return 0;
+}
